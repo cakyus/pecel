12ad47e3b1428cbfb646303ffcaa579419d2932b
cakyus <cakyus@gmail.com>
2024-08-18 23:32:35 +0800

    add execute file

--- pecel.php
+++ pecel.php
@@ -1,2 +1,26 @@
 <?php
 
+exit( main() );
+
+function main(){
+
+	if ($_SERVER["argc"] != 2) {
+		echo "File is required.\n";
+		return 1;
+	}
+
+	$file = $_SERVER["argv"][1];
+
+	if (is_file($file) == false) {
+		echo "File '{$file}' is not found.\n";
+		return 1;
+	}
+
+	include("libpecel.php");
+
+	$pecel = pecel_load_file($file);
+	pecel_exec($pecel);
+
+	return 0;
+}
+
